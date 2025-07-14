6a2879a51eb06242c678a39620457dea3667c998
SergioGBosco <sergiogibosco@gmail.com>
2025-07-14 15:31:00 +0200

    feat: add array of objects

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//creo un array che descrive bici da corsa, con marca/modello e peso, prendo spunto da articoli trovati su Decathlon
+const racingbyke = [
+  {
+    model: `RC 500 Microshift 9V`,
+    kg: 10.5,
+  },
+  {
+    model: `Triban RC120`,
+    kg: 11.5,
+  },
+  {
+    model: `EDR EASY VAN RYSEL `,
+    kg: 11.5,
+  },
+  {
+    model: `TRIBAN RC 500 FB PROWHEEL/SORA`,
+    kg: 11,
+  },
+  {
+    model: ` NCR AF Shimano Tiagra 2x10V`,
+    kg: 9.5,
+  },
+];
+
+
+console.log(racingbyke)
+
+
